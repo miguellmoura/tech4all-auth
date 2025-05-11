067896196d2e4cbc04723bfa244b80b23fac3d47
miguellmoura <miguelmouracvm23@gmail.com>
2025-05-11 18:35:51 -0300

    remove duplicated dependencies

---
+++ src/main/java/auth_tech4all/tech4all/Tech4allApplication.java
@@ -0,0 +1,13 @@
+package auth_tech4all.tech4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4allApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auth_tech4all/tech4all/Tech4allApplicationTests.java
@@ -0,0 +1,13 @@
+package auth_tech4all.tech4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
