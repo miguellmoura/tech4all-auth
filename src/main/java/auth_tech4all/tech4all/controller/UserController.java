c1f63bbd3d101e8a818649565a57b980e1d9db59
miguellmoura <miguelmouracvm23@gmail.com>
2025-06-15 19:46:07 -0300

    test

---
+++ src/main/java/auth_tech4all/tech4all/controller/UserController.java
@@ -0,0 +1,45 @@
+package auth_tech4all.tech4all.controller;
+
+import auth_tech4all.tech4all.model.UserTech4All;
+import auth_tech4all.tech4all.security.Jwt;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<UserTech4All> cadastrarUsuario (@RequestBody UserDTO userDTO) {
+        UserTech4All userTech4All = this.userService.cadastrarUsuario(userDTO);
+        return new ResponseEntity<>(userTech4All, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login (@RequestBody UserLoginDTO userLoginDTO) {
+        LoginResponse loginResponse = userService.login(userLoginDTO);
+
+        if (loginResponse == null) {
+            return new ResponseEntity<LoginResponse>((LoginResponse) null, HttpStatus.UNAUTHORIZED);
+        }
+
+        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/auth_tech4all/tech4all/controller/UserService.java
@@ -0,0 +1,45 @@
+package auth_tech4all.tech4all.controller;
+
+import auth_tech4all.tech4all.model.UserTech4All;
+import auth_tech4all.tech4all.security.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserService {
+
+    private final Jwt jwt;
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository, Jwt jwt) {
+        this.userRepository = userRepository;
+        this.jwt = jwt;
+    }
+
+    public UserTech4All cadastrarUsuario (UserDTO userDTO) {
+        UserTech4All userTech4All = new UserTech4All();
+        userTech4All.setNome(userDTO.getNome());
+        userTech4All.setRole(userDTO.getRole());
+        userTech4All.setContato(userDTO.getContato());
+        userTech4All.setEmail(userDTO.getEmail());
+        userTech4All.setPassword(userDTO.getPassword());
+        userRepository.save(userTech4All);
+        return userTech4All;
+    }
+
+    public LoginResponse login (UserLoginDTO userLoginDTO) {
+        UserTech4All user = userRepository.findByEmail(userLoginDTO.getEmail());
+        if (!Objects.equals(userLoginDTO.getPassword(), user.getPassword())) {
+            return null;
+        }
+        return new LoginResponse(jwt.createToken(user));
+    }
+
+    public void isValid(String token) {
+        return jwt.extract(token);
+    }
+
+}
