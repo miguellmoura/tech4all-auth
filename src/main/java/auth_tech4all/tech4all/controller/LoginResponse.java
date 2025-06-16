8d1a738e4f48f720942531533f6ab1dee6d449b2
miguellmoura <miguelmouracvm23@gmail.com>
2025-06-16 17:00:51 -0300

    adiciona objeto de resposta com dados do usuário

--- src/main/java/auth_tech4all/tech4all/controller/LoginResponse.java
+++ src/main/java/auth_tech4all/tech4all/controller/LoginResponse.java
@@ -2,9 +2,12 @@ package auth_tech4all.tech4all.controller;
 
 public class LoginResponse {
 
+    UserLoginResponseDTO userLoginResponseDTO;
+
     String token;
 
-    public LoginResponse(String token) {
+    public LoginResponse(UserLoginResponseDTO userLoginResponseDTO, String token) {
+        this.userLoginResponseDTO = userLoginResponseDTO;
         this.token = token;
     }
 
@@ -14,5 +17,13 @@ public class LoginResponse {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public UserLoginResponseDTO getUserLoginResponseDTO() {
+        return userLoginResponseDTO;
+    }
+
+    public void setUserLoginResponseDTO(UserLoginResponseDTO userLoginResponseDTO) {
+        this.userLoginResponseDTO = userLoginResponseDTO;
     }
 }
